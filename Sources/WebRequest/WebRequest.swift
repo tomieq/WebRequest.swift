29d7346d0d30978ecce7838ba8d71663a6e05d36
tomieq <tomieq@wp.pl>
2023-09-11 13:59:16 +0200

    Implemented async functions

--- Sources/WebRequest/WebRequest.swift
+++ Sources/WebRequest/WebRequest.swift
@@ -1,13 +1,14 @@
 import Foundation
 
 public struct WebRequestConfig {
-    public static var timeout = 3
+    public static var timeout: TimeInterval = 3
 }
 
 public enum WebRequest<T: Codable> {
     case failure(HttpError)
     case response(T)
 
+    // MARK: sync functions
     public static func get(url: String, headers: [String: String]? = nil) -> WebRequest<T> {
         Self.run(url: url, method: "GET", headers: headers)
     }
@@ -24,20 +25,36 @@ public enum WebRequest<T: Codable> {
         Self.run(url: url, method: "DELETE", body: body.data, headers: headers)
     }
 
+    // MARK: async functions
+
+    public static func get(url: String, headers: [String: String]? = nil, callback: @escaping (WebRequest<T>) -> Void) {
+        _ = Self.run(url: url, method: "GET", headers: headers, callback: callback)
+    }
+
+    public static func post(url: String, body: Codable, headers: [String: String]? = nil, callback: @escaping (WebRequest<T>) -> Void) {
+        _ = Self.run(url: url, method: "POST", body: body.data, headers: headers, callback: callback)
+    }
+
+    public static func put(url: String, body: Codable, headers: [String: String]? = nil, callback: @escaping (WebRequest<T>) -> Void) {
+        _ = Self.run(url: url, method: "PUT", body: body.data, headers: headers, callback: callback)
+    }
+
+    public static func delete(url: String, body: Codable, headers: [String: String]? = nil, callback: @escaping (WebRequest<T>) -> Void) {
+        _ = Self.run(url: url, method: "DELETE", body: body.data, headers: headers, callback: callback)
+    }
+
     private static var defaultHeaders: [String: String] {
         ["Content-Type": "application/json"]
     }
 
-    
     private static func run(url: String, method: String, body: Data? = nil, headers: [String: String]? = nil) -> WebRequest<T> {
-        
         let semaphore = DispatchSemaphore(value: 0)
         var result: WebRequest?
-        let task = run(url: url, method: method, body: body, headers: headers) {
+        let task = self.run(url: url, method: method, body: body, headers: headers) {
             result = $0
             semaphore.signal()
         }
-        let waitResult = semaphore.wait(timeout: .now() + .seconds(WebRequestConfig.timeout))
+        let waitResult = semaphore.wait(timeout: .now() + .seconds(Int(WebRequestConfig.timeout)))
         if case .timedOut = waitResult {
             print("Request timed out")
             task?.cancel()
@@ -50,11 +67,10 @@ public enum WebRequest<T: Codable> {
                             method: String,
                             body: Data? = nil,
                             headers: [String: String]? = nil,
-                            result: @escaping (WebRequest<T>) -> Void) -> URLSessionDataTask? {
-
+                            callback: @escaping (WebRequest<T>) -> Void) -> URLSessionDataTask? {
         print("\(method) \(url)")
         guard let url = URL(string: url) else {
-            result(.failure(.invalidUrl))
+            callback(.failure(.invalidUrl))
             return nil
         }
         var request = URLRequest(url: url)
@@ -67,25 +83,28 @@ public enum WebRequest<T: Codable> {
             }
             return allHeaders
         }()
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let config = URLSessionConfiguration.default
+        config.timeoutIntervalForRequest = WebRequestConfig.timeout
+        config.timeoutIntervalForResource = WebRequestConfig.timeout
+        let task = URLSession(configuration: config).dataTask(with: request) { data, response, error in
             if let error = error as? NSError {
                 let httpError = HttpError.make(from: error.code)
-                result(.failure(httpError))
+                callback(.failure(httpError))
                 return
             }
             guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
-                result(.failure(.other))
+                callback(.failure(.other))
                 return
             }
             if statusCode >= 200, statusCode < 300 {
                 if let json = data, let response = try? T(json: json) {
-                    result(.response(response))
+                    callback(.response(response))
                 } else {
-                    result(.failure(.unserializablaResponse(data)))
+                    callback(.failure(.unserializablaResponse(data)))
                 }
             } else {
                 let body = String(data: data ?? Data(), encoding: .utf8)
-                result(.failure(.invalidHttpCode(code: statusCode, body: body)))
+                callback(.failure(.invalidHttpCode(code: statusCode, body: body)))
             }
         }
         task.resume()
